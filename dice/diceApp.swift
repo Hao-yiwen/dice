41449b532c5c75dd082e9c50b75259b2f9150176
yw.hao <yw.hao@trip.com>
2024-12-04 20:17:59 +0800

    Initial Commit

---
+++ dice/diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceApp.swift
+//  dice
+//
+//  Created by yw.hao on 2024/12/4.
+//
+
+import SwiftUI
+
+@main
+struct diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
